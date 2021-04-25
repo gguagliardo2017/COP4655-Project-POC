46c944c8a0bc5da97996226ac49523de899bad77
Gianu <gguagliardo2017@fau.edu>
2021-04-25 01:30:08 -0500

    working ListView

---
+++ app/src/main/java/com/example/finalprojectp2/Crypto.java
@@ -0,0 +1,59 @@
+package com.example.finalprojectp2;
+
+import com.example.finalprojectp2.ui.home.HomeFragment;
+
+public class Crypto {
+    private String symbol;
+    private String name;
+    private String iconUrl;
+    private String price;
+    private String change;
+
+    public Crypto(String symbol, String name, String iconUrl, String price, String change) {
+        this.symbol = symbol;
+        this.name = name;
+        this.iconUrl = iconUrl;
+        this.price = price;
+        this.change = change;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
+}
---
+++ app/src/main/java/com/example/finalprojectp2/CryptoAdapter.java
@@ -0,0 +1,64 @@
+package com.example.finalprojectp2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CryptoAdapter extends ArrayAdapter<Crypto> {
+
+    private  static final String TAG = "CryptoAdapter";
+    private Context mContext;
+    int mResource;
+    ArrayList mObjects;
+
+    public CryptoAdapter(Context context, int resource, ArrayList<Crypto> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+        mObjects = objects;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        String symbol = getItem(position).getSymbol();
+        String name = getItem(position).getName();
+        String iconUrl = getItem(position).getIconUrl();
+        String price = getItem(position).getPrice();
+        String change = getItem(position).getChange();
+
+        Crypto crypto = new Crypto(symbol, name, iconUrl, price, change);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        convertView = inflater.inflate(mResource, parent, false);
+
+        ImageView CryptoImageView = (ImageView) convertView.findViewById(R.id.CryptoImage);
+        TextView cryptonameView = (TextView) convertView.findViewById(R.id.cryptoName);
+        TextView symbolView = (TextView) convertView.findViewById(R.id.symbol);
+        TextView priceView = (TextView) convertView.findViewById(R.id.price);
+        TextView changeView = (TextView) convertView.findViewById(R.id.change);
+
+        Utils.fetchSvg(getContext(), iconUrl, CryptoImageView);
+        cryptonameView.setText(name);
+        symbolView.setText(symbol);
+        priceView.setText(price);
+        changeView.setText(change + "%");
+
+
+
+
+        return convertView;
+
+
+    }
+}
